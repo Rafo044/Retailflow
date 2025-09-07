af7cebd86efc8522b5fe42138a4cd4b862328b02
Rafo044 <rafo044@protonmail.com>
2025-09-07 23:25:57 +0400

    Add extract transform and load process Close #4

--- dags/etl.py
+++ dags/etl.py
@@ -1,23 +1,71 @@
 
 from pandas.core.reshape.encoding import DataFrame
-from airflow.sdk import task,dag,asset,Asset
+from airflow.sdk import task,dag,asset
 from pandas as pd
-from airflow.models import Variable
+import pandas as pd
+from datetime import datetime
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 
 
 
 
+# ========   Extract Transform  Load  ===========
 
-path = Variable.get("path")
+@dag(
+    schedule="@daily",
+    start_date=datetime(2025, 10, 10),
+    catchup=False,
+    tags=["retail", "etl"]
+)
+def etl():
 
-def extract():
-    df = pd.read_csv(path)
-    return df
+    #Extract
+    @asset(
+    schedule="@daily",
+    uri="file://data/ratildata.csv"
+    )
+    def extract(self)->DataFrame:
+        df = pd.read_csv(self.uri)
+        return df
 
-def transfor():
-    df = extract()
+    #Transform
+    @task()
+    def handling(df : DataFrame) -> DataFrame:
+        if df.isnull().any().any():
+            df.drop_duplicates(subset=['invoice_no'], inplace=True)
+            df.fillna({'payment_method':'Unknown', 'category':'Misc'}, inplace=True)
 
 
+    @task
+    def total_revinue(df : DataFrame) -> DataFrame:
+        df["total_revinue"]= df["quantity"]*df["price"]
+        df["average_order_value"] = df["total_revinue"] / df["invoice_no"]
+        df["total_quantity_solid"] = df["quantity"].sum()
+        return df
 
-def load(df):
-    df.to_csv('transformed_data.csv', index=False)
+    @task
+    def month_weekday(df: DataFrame) -> DataFrame:
+        df["invoice_date"] = pd.to_datetime(df["invoice_date"])
+        df["month"] = df["invoice_date"].dt.month
+        df["weekday"] = df["invoice_date"].dt.day_name()
+        return df
+
+    @task
+    def normalization(df: DataFrame) -> DataFrame:
+        df["price"] = df["price"].astype(float)
+        df["quantity"] = df["quantity"].astype(int)
+        df["total_revinue"] = df["total_revinue"].astype(float)
+        df["average_order_value"] = df["average_order_value"].astype(float)
+        df["total_quantity_solid"] = df["total_quantity_solid"].astype(int)
+        df['category'] = df['category'].str.strip().str.title()
+        return df
+
+    #Load
+    @task
+    def load(df):
+        hook = PostgresHook(postgres_conn_id='retailflow')
+        hook.insert_rows(table='transformed_data', rows=df.to_dict('records'))
+
+   load(normalization(month_weekday(total_revinue(handling(extract())))))
+
+etl()
