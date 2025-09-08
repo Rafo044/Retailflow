35faf845183596b662c5156bc2136545547e6dfa
Rafo044 <rafo044@protonmail.com>
2025-09-08 17:05:50 +0400

    Chenged requirements.txt and compose file

--- dags/etl.py
+++ dags/etl.py
@@ -1,7 +1,6 @@
 
-from pandas.core.reshape.encoding import DataFrame
-from airflow.sdk import task,dag,asset
-from pandas as pd
+from pandas import DataFrame
+from airflow.sdk import task,dag,asset,Asset
 import pandas as pd
 from datetime import datetime
 from airflow.providers.postgres.hooks.postgres import PostgresHook
@@ -12,29 +11,29 @@ from airflow.providers.mysql.hooks.mysql import MySqlHook
 
 # ========   Extract Transform  Load  ===========
 
+
+@asset(
+    schedule="@daily",
+    uri="https://raw.githubusercontent.com/Rafo044/Retailflow/refs/heads/main/data/retaildata.csv"
+)
+def extract(self)->DataFrame:
+    df = pd.read_csv(self.uri)
+    return df
+
+
 @dag(
     schedule="@daily",
     start_date=datetime(2025, 10, 10),
     catchup=False,
     tags=["retail", "etl"]
 )
-def etl():
-
-    #Extract
-    @asset(
-    schedule="@daily",
-    uri="file://data/ratildata.csv"
-    )
-    def extract(self)->DataFrame:
-        df = pd.read_csv(self.uri)
-        return df
-
-    #Transform
+def etl(df): #Transform
     @task()
     def handling(df : DataFrame) -> DataFrame:
         if df.isnull().any().any():
             df.drop_duplicates(subset=['invoice_no'], inplace=True)
             df.fillna({'payment_method':'Unknown', 'category':'Misc'}, inplace=True)
+            return df
 
 
     @task
@@ -67,6 +66,7 @@ def etl():
         hook = MySqlHook(mysql_conn_id='retailflow')
         hook.insert_rows(table='transformed_data', rows=df.to_dict('records'))
 
-   load(normalization(month_weekday(total_revinue(handling(extract())))))
+    load(normalization(month_weekday(total_revinue(handling()))))
 
-etl()
+
+etl(extract())
